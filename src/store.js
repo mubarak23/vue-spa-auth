d0d9f9f96c7a8c7376e8f67a577fdfeb94d0554c
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-11-25 11:57:35 +0100

    create a mutuation that store the tokenid and userid

--- src/store.js
+++ src/store.js
@@ -10,23 +10,39 @@ export default new Vuex.Store({
   	userId: null
   },
   mutations: {
-  	
+  	authUser (state, userData){
+  			state.IdToken = userData.token,
+  			state.userId = userData.userId
+  	},
+
   },
   actions: {
-  	signup({commit}, authData){
+  	signup ({commit}, authData){
   		axios.post('/signupNewUser?key=AIzaSyBH-Rgu4Kw13Cn__dy8ZUTjf_egolhnYPs', {
           email: authData.email,
           password: authData.password,
           returnSecureToken: true
-        }).then(res => console.log(res))
+        }).then(res => {
+        	console.log(res),
+        	commit('authUser', {
+        		token: res.data.idToken,
+        		userId: res.data.localId
+        	})
+        })
         .catch(error => console.log(error));
   	},
-  	login({commit}, authData){
+  	login ({commit}, authData){
   		 axios.post('/verifyPassword?key=AIzaSyBH-Rgu4Kw13Cn__dy8ZUTjf_egolhnYPs', {
           email: authData.email,
           password: authData.password,
           returnSecureToken: true
-        }).then(res => console.log(res))
+        }).then(res => {
+        	console.log(res),
+        	commit('authUser', {
+        		token: res.data.idToken,
+        		userId: res.data.localId
+        	})
+        })
         .catch(error => console.log(error));
   	}
   },
