f860c7e992547130ee2a22f5c58fe2b336df7a6f
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-12-06 23:05:58 +0100

    use the sameAs to check password and confirmpassword field

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -21,18 +21,20 @@
                   v-model.number="age">
                   <p v-if="!$v.age.minVal">You have to be {{ $v.age.$params.minVal.min }} Years Old</p>
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.password.$error}">
           <label for="password">Password</label>
           <input
                   type="password"
                   id="password"
+                  @blur="$v.password.$touch()"
                   v-model="password">
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.confirmPassword.$error}">
           <label for="confirm-password">Confirm Password</label>
           <input
                   type="password"
                   id="confirm-password"
+                  @blur="$v.confirmPassword.$touch()"
                   v-model="confirmPassword">
         </div>
         <div class="input">
@@ -75,7 +77,7 @@
 
 <script>
 import axios from 'axios'
-import { required, email, numeric, minValue} from 'vuelidate/lib/validators'
+import { required, email, numeric, minValue, minLength, sameAs } from 'vuelidate/lib/validators'
   export default {
     data () {
       return {
@@ -97,6 +99,13 @@ import { required, email, numeric, minValue} from 'vuelidate/lib/validators'
         required : required,
         age : numeric,
         minVal: minValue(18)
+      },
+      password:{
+      required :required,
+      minLen: minLength(6)
+      },
+      confirmPassword:{
+        sameAs: sameAs('password')
       }
     },
     methods: {
